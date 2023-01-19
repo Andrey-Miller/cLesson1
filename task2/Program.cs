0ae234beb16336a92e782de8dac0c25d73586cae
sinedd <sineddam@mail.ru>
2023-01-19 12:58:16 +0500

    tasks added

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введи первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if ((num1>num2) && (num1>num3))
+{
+    max = num1;
+}
+else if ((num2>num1) && (num2>num3))
+{
+    max = num2;
+}
+else if ((num3>num1) && (num3>num2))
+{
+    max = num3;
+}
+else {
+    max = num1;
+    //если попали сюда, то все 3 числа равны, поэтому max - любое из 3х чисел
+}
+Console.WriteLine($"Наибольшее число: {max}");
---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введи число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 0) {
+    Console.WriteLine($"Нужно ввести число больше 0");
+    System.Environment.Exit(0);
+}
+Console.WriteLine($"Четные числа от 1 до {num}:");
+for (int i = 2; i <= num; i+=2)
+{
+    Console.WriteLine(i);
+}
